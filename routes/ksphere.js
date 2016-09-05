53e609596f11d3bf4b913efdb19455db1fb6a9d8
Shahar Solomianik <shahar@gnift.com>
2016-09-05 11:23:47 +0300

    many changes, works nice now

--- routes/ksphere.js
+++ routes/ksphere.js
@@ -17,7 +17,9 @@ var marked = require('marked');
 
 router.post('/', function(req, res, next) {
   console.log('recieved query: %s',req.body.text)
-  var users = {};
+  // var users = {};
+  var usersArray = [];
+  var answer = '';
   if(req.body.token != config.get('slack.command_token')){
     console.log('somebody spoofing us!')
     res.sendStatus(500);
@@ -28,20 +30,53 @@ router.post('/', function(req, res, next) {
         res.sendStatus(500)
       }else{
         console.log('matches are: %s',util.inspect(matches))
-        _.each(matches,function(match){
-          if(match.user){
-            if(!(match.username in users)){
-              users[match.username] = 1;
-            }else{
-              users[match.username] += 1;
-            }
-          }
-        })
-        console.log('users list is: %s',util.inspect(users))
-        var bestUser = Object.keys(users).reduce(function(a, b){ return users[a] > users[b] ? a : b });
-        console.log('sorted users list is: %s',util.inspect(bestUser))
+        if(matches.length == 0){
+          answer = util.format('Sorry pal. Nobody knows anything about *%s*...',req.body.text)
+        }else{
+          _.each(matches,function(match){
+            if(match.user){
 
-        request.post(req.body.response_url,{body: JSON.stringify({text: bestUser})},function(error,response,body){
+              // do we have this user in this array already?
+              var userIndex = _.findIndex(usersArray,function(user){
+                return user.username == match.username
+              });
+
+              if(userIndex == -1){
+                usersArray.push({
+                  username: match.username,
+                  user_id: match.user,
+                  count: 1
+                })
+              }else{
+                usersArray[userIndex].count += 1;
+              }
+
+              // if(!(match.username in users)){
+              //   users[match.username] = 1;
+              // }else{
+              //   users[match.username] += 1;
+              // }
+            }
+          })
+
+          // sort the array accoridng to count desc
+          usersArray = _.sortBy(usersArray,'count').reverse();
+
+          console.log('users array is: %s',util.inspect(usersArray));
+
+          var maxIndex = usersArray.length > 3 ? 3 : usersArray.length;
+          answer += util.format('Your best matches for *%s* are:\n',req.body.text);
+          for(var i=0;i<maxIndex;i++){
+            answer += util.format('%d. <@%s|%s>\n',i+1,usersArray[i].user_id,usersArray[i].username);
+          }
+
+          // console.log('users list is: %s',util.inspect(users))
+          // var bestUser = Object.keys(users).reduce(function(a, b){ return users[a] > users[b] ? a : b });
+          // console.log('sorted users list is: %s',util.inspect(bestUser))
+          // answer = bestUser;
+        }
+
+        request.post(req.body.response_url,{body: JSON.stringify({text: answer})},function(error,response,body){
           if(error){
             console.log('error posting answer to slack: %s',error)
     				res.sendStatus(500)
