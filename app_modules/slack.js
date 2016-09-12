6f0bb5b3bfe9528ec5dd96159a6888ab2116283f
Shahar Solomianik <shahar@gnift.com>
2016-09-12 12:38:49 +0300

    shit works!

--- app_modules/slack.js
+++ app_modules/slack.js
@@ -1,32 +1,44 @@
 var async = require('async')
+var util = require('util')
+var request = require('request')
 
 module.exports = {
   handlePayload: function(db,payload,callback){
+
 
     async.waterfall([
       function(callback){
         var users = db.get('users');
         users.findOne({user_id: payload.user.id},function(err,user){
+          console.log('user is %s',util.inspect(user))
           callback(err,user)
         })
       },
-      function(callback){
+      function(user,callback){
         var questions = db.get('questions');
         questions.findOne({_id: payload.callback_id},function(err,question){
+          console.log('q is %s',util.inspect(question))
           callback(err,user,question)
         })
+      },
+      function(user,question,callback){
+        console.log('payload is %s',util.inspect(payload))
+        if(payload.actions[0].value == 'channel'){
+          processChannel(user.access_token,question,function(err){
+            callback(err)
+          })
+        }else if(payload.actions[0].value == 'group'){
+          processGroup(user.access_token,question,function(err){
+            callback(err)
+          })
+        }else{
+          callback('unrecognized action')
+        }
       }
     ],function(err){
-
+      callback(err)
     })
 
-    console.log('HERE')
-    callback()
-    // async.waterfall([
-    //
-    // ],function(err){
-    //
-    // })
   }
 }
 
@@ -38,11 +50,13 @@ function processChannel(accessToken,question,callback){
         token: accessToken,
         name: util.format('KSphere %s',question._id.toString())
       }
+      console.log('AAA')
       request.post('https://slack.com/api/channels.join',{form: form},function(error,response,body){
         if(error){
           callback(error)
         }else if(response.statusCode > 300){
           callback(response.statusCode + ' : ' + body)
+// console.log(response.statusCode + ' : ' + body)
         }else{
           var data = JSON.parse(body)
           callback(null,data.channel)
@@ -56,7 +70,28 @@ function processChannel(accessToken,question,callback){
         channel: channel.id,
         topic: question.slack.text
       }
+      console.log('BBB')
       request.post('https://slack.com/api/channels.setTopic',{form: form},function(error,response,body){
+        if(error){
+          callback(error)
+        }else if(response.statusCode > 300){
+          callback(response.statusCode + ' : ' + body)
+        }else{
+          var data = JSON.parse(body)
+          callback(null,channel)
+        }
+      })
+
+    },
+    // set purpose
+    function(channel,callback){
+      var form = {
+        token: accessToken,
+        channel: channel.id,
+        purpose: util.format('To discuss my question: %s',question.slack.text)
+      }
+      console.log('BBB')
+      request.post('https://slack.com/api/channels.setPurpose',{form: form},function(error,response,body){
         if(error){
           callback(error)
         }else if(response.statusCode > 300){
@@ -70,7 +105,26 @@ function processChannel(accessToken,question,callback){
     },
     // invite matched ppl
     function(channel,callback){
+      console.log('CCC')
+      async.each(question.matched_users,function(matchedUser){
+        var form = {
+          token: accessToken,
+          channel: channel.id,
+          user: matchedUser.user_id
+        }
+        request.post('https://slack.com/api/channels.invite',{form: form},function(error,response,body){
+          if(error){
+            callback(error)
+          }else if(response.statusCode > 300){
+            callback(response.statusCode + ' : ' + body)
+          }else{
+            callback(null)
+          }
+        })
 
+      },function(err){
+        callback(err)
+      })
     },
   ],function(err){
     callback(err)
