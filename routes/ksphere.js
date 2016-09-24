a7c81616f6450a9f8c5fd949268482e35459d7b2
Shahar Solomianik <shahar@gnift.com>
2016-09-24 21:17:51 +0300

    many things for slack app approval

--- routes/ksphere.js
+++ routes/ksphere.js
@@ -31,6 +31,12 @@ router.post('/', function(req, res, next) {
         console.log('error answering slack: %s',err)
       }
     })
+  }else if(req.body.text == 'help'){
+    answerSlack(req.body.response_url,{text: 'Finds team members who are most likely to answer your question\nUSage: /whoknows {question}'},function(err){
+      if(err){
+        console.log('error answering slack: %s',err)
+      }
+    })
   }else{
 
     async.waterfall([
